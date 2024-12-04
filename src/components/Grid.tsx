5c69a73f59862267e0036a504b020ec670f7265d
544-eth <elioku20@gmail.com>
2024-12-04 17:43:24 +0100

    initial commit

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -8,15 +8,20 @@ const Gridex = () => {
       <BentoGrid >
         
           {gridItems.map
-          ((item) => (
+          (({ id, title, description, className, img, imgClassName, titleClassName, spareImg }) => (
             <BentoGridItem
-              id={item.id}
-              key={item.id}
-              title={item.title}
-              description={item.description}
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
             />
           ))}
-          
+
       </BentoGrid>
     </section>
   )
--- src/components/ui/BentoGrid.tsx
+++ src/components/ui/BentoGrid.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
-import { Globe } from "./GridGlobee";
+import { GlobeDemo } from "./GridGlobe";
+import { div } from "three/webgpu";
 
 export const BentoGrid = ({
   className,
@@ -25,8 +26,6 @@ export const BentoGridItem = ({
   className,
   title,
   description,
-  header,
-  icon,
   img,
   imgClassName,
   titleClassName,
@@ -36,27 +35,76 @@ export const BentoGridItem = ({
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
-  header?: React.ReactNode;
-  icon?: React.ReactNode;
   img?: string;
   imgClassName?: string;
   titleClassName?: string;
   spareImg?: string;
-  id?: number,
+  id?: number;
 }) => {
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white  border-transparent justify-between flex flex-col space-y-4 border",
+        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white  border-transparent justify-between flex flex-col space-y-4 border border-red-500",
         className
       )}
       style={{
-        background: 'rgb(4, 7, 29)',
+        background: 'rgb(4, 7, 29)', 
         backgroundColor: 'linear-gradient(90deg, rgba(4,7,29,1) 0% rgba(12,14,35,1) 100%)',
       }}
     >
 
-      
+      <div className={`${id === 6} && 'flex justify-center h-full'`}>
+            <div className=" w-full h-full absolute">
+              {img && (
+                <img
+                  src={img}
+                  alt={img}
+                  className={cn(imgClassName, 'object-cover, object-center')}
+                />
+              )}
+            </div>
+            <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
+              {spareImg && (
+                <img
+                  src={spareImg}
+                  alt={spareImg}
+                  className={'object-cover, object-center w-full h-full'}
+              />
+              )}         
+            </div>
+              {id === 6 && (
+                <BackgroundGradientAnimation>
+                  <div className=" absolute z-50 flex items-center justify-center text-white font-bold" /> 
+                </BackgroundGradientAnimation>
+              )}
+
+            <div className={cn(
+              titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
+            )}>
+              <div className="font-sans font-extralight text-[#c1c2d3]  text-sm md:text-xs lg:text-base z-10">
+                {description}
+              </div>
+              <div className=" font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
+                {title}
+              </div>
+            
+            {id === 2 && <GlobeDemo/>}
+
+            {id === 3 && (
+                  <div className=" flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+                    <div className=" flex flex-col gap-3 lg:gap-8">
+                      {['React.js', 'Next.js', 'TypeScript'].map
+                      ((item) => (
+                        <span key={item} className=" py-2 lg:py-4 lg:px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                          {item}
+                        </span>
+                      ))}
+                    </div>
+                  </div>
+                  ) }
+
+          </div> 
+      </div>
     </div>
   );
 };
